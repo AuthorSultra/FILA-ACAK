12e4058734073f3d7fdbceb65f343f6c85184cbb
AuthorSultra <128295202+AuthorSultra@users.noreply.github.com>
2024-06-13 09:27:34 +0000

    Add files via upload

---
+++ config/config.candy.php
@@ -0,0 +1,9 @@
+<?php
+$copyright = 'Candy CBT';
+
+define("VERSI", "2.9.4");
+define("REVISI", "1");
+define("APLIKASI", "Candy CBT");
+define("NAMA_DATABASE", "crack-cbt");
+define("VERSI_DB", "2.9.4");
+define("BASEPATH", "cbtcandy");
